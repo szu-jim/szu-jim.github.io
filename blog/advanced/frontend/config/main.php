c45d66ce8692224c7ea955db7da119595e98b494
jim-home <812329595@qq.com>
2017-06-18 00:35:13 +0800

    yii2 configure

---
+++ blog/advanced/frontend/config/main.php
@@ -0,0 +1,67 @@
+<?php
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php'),
+    require(__DIR__ . '/params-local.php')
+);
+
+return [
+    'id' => 'app-frontend',
+    'basePath' => dirname(__DIR__),
+    'language' => 'zh-CN',
+    'bootstrap' => ['log'],
+    'controllerNamespace' => 'frontend\controllers',
+    'components' => [
+        'request' => [
+            'csrfParam' => '_csrf-frontend',
+        ],
+        'user' => [
+            'identityClass' => 'common\models\UserModel',
+            'enableAutoLogin' => true,
+            'identityCookie' => ['name' => '_identity-frontend', 'httpOnly' => true],
+        ],
+        'session' => [
+            // this is the name of the session cookie used for login on the frontend
+            'name' => 'advanced-frontend',
+        ],
+        /*
+        'urlManger' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+        ],
+        */
+        'i18n' => [
+            'translations' => [
+                '*' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    //'basePath' => '/Message',
+                    'fileMap' => [
+                        'common' => 'common.php',
+                    ],
+                ],
+            ],
+        ],    
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        /*
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+            ],
+        ],
+        */
+    ],
+    'params' => $params,
+];
---
+++ blog/advanced/frontend/messages/zh-CN/common.php
@@ -0,0 +1,10 @@
+<?php
+return[
+	'Blog' => '博客',
+	'Home' => '主页',
+	'About' => '关于',
+	'Contact' => '联系',
+	'Signup' => '注册',
+	'Login' => '登录',
+	'Logout' => '登出',
+];
